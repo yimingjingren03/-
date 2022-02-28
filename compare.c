0d96bbc31b8a3129773dc38233f94cb40ab88718
yimingjingren03 <100463630+yimingjingren03@users.noreply.github.com>
2022-02-28 23:29:35 +0800

    Create compare.c
    比较数字，求中间数
    比较字符串，求中间长

---
+++ compare.c
@@ -0,0 +1,15 @@
+//最大值
+((a<b?b:a)<(a<c?c:a)):
+//最小值
+
+//中间值
+a<b?(a>c?a:(c<b?c:b));:(a<c?a:(b>c?b:c))
+
+//最大字符串
+strcmp(a,b)>0?(strcmp(a,c)>0?a:c):(strcmp(b,c)>0?b:c)
+//最小字符串
+strcmp(a,b)<0?(strcmp(a,c)<0?a:c):(strcmp(b,c)<0?b:c)]
+//中间字符串
+strcmp(a,b)>0?(strcmp(a,c))
+
+//还没完，明天来。
